fefd7c66826791cc855512469d7be54e15adaa6e
sorrymint <ldmatheis.00@gmail.com>
2025-10-28 12:33:52 -0500

    Changed playwright.config.js to report JUnit XML report for GitHub, also worked on Workflow.

--- playwright.config.js
+++ playwright.config.js
@@ -1,27 +1,31 @@
-import { defineConfig, devices } from "@playwright/test"
+import {defineConfig, devices} from "@playwright/test"
 
 export default defineConfig({
-  testDir: "./tests",
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
-  use: {
-    baseURL: "http://localhost:5173",
-    trace: "on-first-retry",
-  },
-
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+    testDir: "./tests",
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: [
+        ["list"],
+        ['junit', { outputFile: 'playwright-report/results.xml' }],
+        ['html', { outputFolder: 'playwright-report/html' }],
+    ],
+    use: {
+        baseURL: "http://localhost:5173",
+        trace: "on-first-retry",
     },
-  ],
 
-  webServer: {
-    command: "npm run dev",
-    url: "http://localhost:5173",
-    reuseExistingServer: !process.env.CI,
-  },
+    projects: [
+        {
+            name: "chromium",
+            use: {...devices["Desktop Chrome"]},
+        },
+    ],
+
+    webServer: {
+        command: "npm run dev",
+        url: "http://localhost:5173",
+        reuseExistingServer: !process.env.CI,
+    },
 })
